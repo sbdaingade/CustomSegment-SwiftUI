84d9242346a241ab0c0e38041b0c1da550a22eca
Sachin Daingade <sbdaingade@gmail.com>
2021-11-08 23:10:43 +0530

    first comment

---
+++ CustomSegment-SwiftUI/CustomSegment-SwiftUI/CustomSegment_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegment_SwiftUIApp.swift
+//  CustomSegment-SwiftUI
+//
+//  Created by Sachin Daingade on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegment_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
